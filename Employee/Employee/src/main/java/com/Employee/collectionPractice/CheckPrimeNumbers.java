e1b77145ce2d9dfa70aa45822c75f06857f4862b
Debasish Mohanta <219381354+Debasish-Java-Developer@users.noreply.github.com>
2026-01-14 08:13:24 +0530

    Practice Code

---
+++ Employee/Employee/src/main/java/com/Employee/collectionPractice/CheckPrimeNumbers.java
@@ -0,0 +1,15 @@
+package com.Employee.collectionPractice;
+
+import java.util.stream.IntStream;
+
+public class CheckPrimeNumbers {
+    public static void main(String[] args) {
+        int n = 20; // Upper limit
+        IntStream.rangeClosed(2, n)                          // Numbers from 2 to n
+                .filter(num ->                          // Filter prime numbers
+                        IntStream.range(2, num)             // Check divisors
+                                .noneMatch(i -> num % i == 0))
+                .forEach(num -> System.out.print(num + " ")); // Print primes
+    }
+}
+
